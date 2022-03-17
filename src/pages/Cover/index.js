372fcccd90b1b3c8210743c43c4b96c0f66503f8
r4920 <rahulhingmire65@gmail.com>
2022-03-17 05:44:51 +0000

    Code Synced by DhiWise

--- src/pages/Cover/index.js
+++ src/pages/Cover/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Column } from "components/Column";
 import { Row } from "components/Row";
 import { Text } from "components/Text";
-import { Image } from "components/Image";
+import { Input } from "components/Input";
 
 const CoverPage = () => {
   return (
@@ -13,11 +13,11 @@ const CoverPage = () => {
           <Text className="2xl:ml-[60px] 2xl:text-fs23 3xl:ml-[72px] 3xl:text-fs27 font-normal lg:ml-[47px] lg:text-fs18 ml-[81px] opacity-op5 text-fs31 text-left text-white_A700 xl:ml-[54px] xl:text-fs20">{`UX/UI`}</Text>
           <Text className="2xl:mr-[60px] 2xl:text-fs23 3xl:mr-[72px] 3xl:text-fs27 font-normal lg:mr-[47px] lg:text-fs18 mr-[81px] opacity-op5 text-fs31 text-right text-white_A700 xl:mr-[54px] xl:text-fs20">{`2021`}</Text>
         </Row>
-        <Image
-          src="img_telegram11.svg"
-          className="2xl:h-[154px] 2xl:mt-[135px] 3xl:h-[184px] 3xl:mt-[162px] h-[204px] lg:h-[119px] lg:mt-[105px] max-w-[204px] mt-[181px] mx-[auto] object-contain w-[auto] xl:h-[137px] xl:mt-[120px]"
-          alt="telegram11"
-        />
+        <Input
+          className="2xl:mt-[135px] 3xl:mt-[162px] bg-transparent lg:mt-[105px] max-w-[204px] mt-[181px] mx-[auto] w-[11%] xl:mt-[120px]"
+          name="telegram11"
+          placeholder=""
+        ></Input>
         <Text className="2xl:mt-[63px] 2xl:text-fs98 3xl:mt-[76px] 3xl:text-fs117 font-semibold lg:mt-[49px] lg:text-fs76 max-w-[1040px] mt-[85px] mx-[auto] text-center text-fs131 text-white_A700 tracking-ls1 w-[55%] xl:mt-[56px] xl:text-fs87">{`Telegram Contest`}</Text>
         <Text className="2xl:mt-[8px] 2xl:text-fs35 3xl:mt-[9px] 3xl:text-fs42 font-normal lg:mt-[6px] lg:text-fs27 max-w-[196px] mt-[11px] mx-[auto] opacity-op5 text-center text-fs47 text-white_A700 w-[11%] xl:mt-[7px] xl:text-fs31">{`@dikaleto`}</Text>
       </Column>
